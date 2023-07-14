0a2fcef43275b7301a71d9dd1aca7c35a4e055e6
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-07-14 11:10:41 +0700

    3rd commit

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -5,22 +5,29 @@ import {useNavigate} from "react-router-dom";
 function Users() {
     const [users, setUsers] = useState([]);
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/api/users')
-            .then(res => {
-                setUsers(res.data);
-            })
-            .catch(err => {
-                throw err;
-            });
+        setLoading(true);
+        setTimeout(() => {
+            axios
+                .get('http://localhost:3001/api/users')
+                .then(res => {
+                    setUsers(res.data);
+                })
+                .catch(err => {
+                    throw err;
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        }, 3000);
     }, []);
 
     const handleCreate = () => {
         navigate('/users/add');
     }
-
+    if (loading) return <p>Loading...</p>
     return (
         <div>
             <h1>User</h1>
